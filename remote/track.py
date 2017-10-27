bf3d9c5fb1dff14489864da11af9f14ecd94a9d9
BlackVirusScript <blackvirusscript@tutanota.com>
2017-10-26 22:02:20 -0200

    Update track.py

--- remote/track.py
+++ remote/track.py
@@ -13,18 +13,18 @@ class Track(Program):
         response = requests.get(f"http://ip-api.com/json/{self.arguments.host}?fields=258047").json()
         assert response["status"] == "success", Exception(f"{' - '.join(f'{key.title()}: {value}' for key, value in response.items())}")
         print("""            ,,,,,,                 
-        o#'9MMHb':'-,o,            --------- Tracking Results ---------
-     .oH":HH$' "' ' -*R&o,                 IP Address {query}
-    dMMM*""'`'      .oM"HM?.                  Country {country}/{countryCode}
-  ,MMM'          "HLbd< ?&H                    Region {regionName}/{region}
- .:MH ."          ` MM  MM&b                     City {city}
-. "*H    -        &MMMMMMMMMH:               Zip Code {zip}
-.    dboo        MMMMMMMMMMMM.              Time Zone {timezone}
-.   dMMMMMMb      *MMMMMMMMMP.           Organization {org}
-.    MMMMMMMP        *MMMMMP .                    ISP {isp}
-     `#MMMMM           MM6P ,                Latitude {lat}
- '    `MMMP"           HM*`,                Longitude {lon}
-  '    :MM             .- ,                  Is Proxy {proxy}
-   '.   `#?..  .       ..'                  Is Mobile {mobile}
-      -.   .         .-                     {as}
-        ''-.oo,oo.-''              """.format(**response))
+        o#'9MMHb':'-,o,               --------- Tracking Results ---------
+     .oH":HH$' "' ' -*R&o,                    IP Address {query}
+    dMMM*""'`'      .oM"HM?.                     Country {country}/{countryCode}
+  ,MMM'          "HLbd< ?&H                       Region {regionName}/{region}
+ .:MH ."          ` MM  MM&b                        City {city}
+. "*H    -        &MMMMMMMMMH:                  Zip Code {zip}
+.    dboo        MMMMMMMMMMMM.                 Time Zone {timezone}
+.   dMMMMMMb      *MMMMMMMMMP.              Organization {org}
+.    MMMMMMMP        *MMMMMP .                       ISP {isp}
+     `#MMMMM           MM6P ,                   Latitude {lat}
+ '    `MMMP"           HM*`,                   Longitude {lon}
+  '    :MM             .- ,                     Is Proxy {proxy}
+   '.   `#?..  .       ..'                     Is Mobile {mobile}
+      -.   .         .-                        {as}
+        ''-.oo,oo.-''                 """.format(**response))
